7d68da4d726f3c3da64325e26f5bfcba7e8732fe
amine <mraminewiebke@gmail.com>
2023-09-30 16:17:55 +0200

    make it better

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ window.onscroll = function(){
     let value = scrollY;
     console.log(value)
     stars.style.left = value + 'px';
-    moon.style.top = value * 4 + 'px' ;
+    moon.style.top = value * 3 + 'px' ;
     mountains3.style.top = value *2 + 'px';
     mountains4.style.top = value *1.5 + 'px';
     river.style.top = value + 'px'
